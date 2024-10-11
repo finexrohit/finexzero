f818f9cfea204c201f3e6eaa35056b4cf6a9a093
Rohit Saini <148010519+finexrohit@users.noreply.github.com>
2024-10-11 12:47:47 +0530

    Add files via upload

---
+++ D.S.A Programs/Array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ int main() 
+ {
+    int A [5]={7,3,9,2,0};
+
+    int B [5]={ 2, 3, 5, 6, 8};
+
+    int i;
+
+    for ( i = 0; i < 5; i++)
+
+    {if(B[i]<5)
+    
+    {
+        printf("%d ",B[i]);}
+        
+    }
+    
+return 0;
+ }
