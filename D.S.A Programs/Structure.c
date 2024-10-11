a3cd2d3a3d0a0db10e804bdd8bb7380649e997d3
Rohit Saini <148010519+finexrohit@users.noreply.github.com>
2024-10-11 12:48:07 +0530

    Add files via upload

---
+++ D.S.A Programs/Structure.c
@@ -0,0 +1,16 @@
+    #include<stdio.h>
+
+    int main() {
+
+    struct Rectangle ;
+    {
+    int length  = 15;
+    int breadth = 20;
+    };
+
+    printf("Area Of Rectangle is %d" );
+    printf( area Rectangle) ;
+
+    return 0; 
+
+    }
