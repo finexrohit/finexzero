d82f7ed73b97712acae9f29acc75488bf456e438
Rohit Saini <148010519+finexrohit@users.noreply.github.com>
2024-11-12 23:00:51 +0530

    Add files via upload

---
+++ Oops in C++/For_Loop.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+// For Loop Normal Example
+
+for (int i = 1; i < 10; i++)
+{
+    cout<<i<<endl;
+}
+
+// For Loop Infinite Example
+
+for (int r = 1; 6 < 10; r++)
+{
+    cout<<r<<endl;
+}
+
+
+return 0;
+
+}
