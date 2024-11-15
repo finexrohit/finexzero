28bc72d2888544789d8d89158c60e3ef51a97aa7
Rohit Saini <148010519+finexrohit@users.noreply.github.com>
2024-11-15 19:53:49 +0530

    Add files via upload

---
+++ Oops in C++/While_Loop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+// Normal Example For While Loop    
+int i = 1;
+
+while (i<=5)
+{
+    cout<<i<<endl;
+    i++;
+}
+// Example For Infinite While Loop
+
+while (true)
+{
+    cout<<i<<endl;
+    i++;
+}
+
+return 0;
+
+}
