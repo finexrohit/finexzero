19e172a55e2e8b8bd5672d12c8eb6d0e19b6223c
Rohit Saini <148010519+finexrohit@users.noreply.github.com>
2025-03-08 23:29:34 +0530

    Add files via upload

---
+++ Java Programs/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    public static void main (String[] args)
+    {
+        String name ="Rohit";
+        String neighbour = "Roni";
+        String friend = neighbour;
+        int age = 21;
+
+        System.out.println(name);
+        System.out.println(neighbour);
+        System.out.println(friend);
+        System.out.println(age);
+
+    }
+}
